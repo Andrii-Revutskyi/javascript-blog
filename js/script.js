1eae0e89779bcfc8813e4ac4244a781713c79357
Andrii Revutskyi <andrew31101997@gmail.com>
2025-04-16 17:24:36 +0200

    Finish titleClickHandler function

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ function titleClickHandler(event){
   const clickedElement = this;
   console.log('Link was clicked!');
 
-  /* [DONE] remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links */
   const activeLinks = document.querySelectorAll('.titles a.active');
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
-  clickedElement.classList.add('active');
+  /* [DONE] add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
@@ -22,13 +22,16 @@ function titleClickHandler(event){
   }
 
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
-
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+}
